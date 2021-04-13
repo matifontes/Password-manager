c03a791cf5d1f6c7196361252eb5780dcf28467f
Renzo Matonti <matontirenzo@gmail.com>
2021-04-13 17:38:19 -0300

    code: Create Unit Test UserTest.cs

---
+++ PasswordManager/PasswordManagerTest/UserTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PasswordManager;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void CreateUser()
+        {
+            User profile = new User();
+            Assert.IsNotNull(profile);
+        }
+    }
+}
