0c8194420a5c286cfe58d66dc991ab8fcae2d905
Matias Fontes <matifontes13@gmail.com>
2021-04-15 15:01:19 -0300

    refactor: Password constructor receives parameters

--- PasswordManager/PasswordManager/Password.cs
+++ PasswordManager/PasswordManager/Password.cs
@@ -8,8 +8,18 @@ namespace PasswordManager
 {
     public class Password
     {
-        public Password()
+        public Categorie Categorie { get; set; }
+        public string Pass { get; set; }
+        public string Site { get; set; }
+        public string User { get; set; }
+        public string Note { get; set; }
+        public Password(Categorie categorie, string password, string site, string user, string note)
         {
+            this.Categorie = categorie;
+            this.Pass = password;
+            this.Site = site;
+            this.User = user;
+            this.Note = note;
         }
     }
 }
