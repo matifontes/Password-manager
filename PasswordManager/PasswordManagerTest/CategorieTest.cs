78d34b77398c536a57f57625fe4465e433baa476
Matias Fontes <matifontes13@gmail.com>
2021-04-15 13:28:05 -0300

    code: Create Unit Test CategorieTest.cs

---
+++ PasswordManager/PasswordManagerTest/CategorieTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordManager;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class CategorieTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
