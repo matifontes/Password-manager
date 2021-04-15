ed4d5f0272e6fa9294577ac364a92214d972d8e3
Renzo Matonti <matontirenzo@gmail.com>
2021-04-15 15:01:05 -0300

    code: Basic implementation of categories on System class RED TESTS

--- PasswordManager/PasswordManager/System.cs
+++ PasswordManager/PasswordManager/System.cs
@@ -1,16 +1,25 @@
-﻿namespace PasswordManager
+﻿using System.Collections;
+
+namespace PasswordManager
 {
     public class System
     {
         public string SystemPassword {get; set;}
+        private ArrayList categories;
 
         public System(string password) {
             this.SystemPassword = password;
+            categories = new ArrayList();
         }
 
         public bool Login(string password) 
         {
             return password.Equals(SystemPassword);
+        }
+
+        public void AddCategorie(Categorie categorie) 
+        {
+
         }
     }
 }
