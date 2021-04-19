6380221417bb3db83b19a5a74740ae5c49501a14
Matias Fontes <matifontes13@gmail.com>
2021-04-19 13:22:44 -0300

    refactor_ changed categorie to category on Password.cs

--- PasswordManager/PasswordManager/Password.cs
+++ PasswordManager/PasswordManager/Password.cs
@@ -8,14 +8,14 @@ namespace PasswordManager
 {
     public class Password
     {
-        public Categorie Categorie { get; set; }
+        public Category Category { get; set; }
         public string Pass { get; set; }
         public string Site { get; set; }
         public string User { get; set; }
         public string Note { get; set; }
-        public Password(Categorie categorie, string password, string site, string user, string note)
+        public Password(Category categorie, string password, string site, string user, string note)
         {
-            this.Categorie = categorie;
+            this.Category = categorie;
             this.Pass = password;
             this.Site = site;
             this.User = user;
