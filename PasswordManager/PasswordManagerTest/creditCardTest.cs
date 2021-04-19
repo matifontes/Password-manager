7fe0babfb015f04337909ceee345e0b97f6c04e2
Renzo Matonti <matontirenzo@gmail.com>
2021-04-19 09:56:32 -0300

    code: Added parameters for CreditCard on CreditCardTest RED TEST

--- PasswordManager/PasswordManagerTest/creditCardTest.cs
+++ PasswordManager/PasswordManagerTest/creditCardTest.cs
@@ -10,7 +10,15 @@ namespace PasswordManagerTest
         [TestMethod]
         public void CreateCreditCard()
         {
-            CreditCard card = new CreditCard();
+            Category category = new Category("Work");
+            string name = "Visa Gold";
+            string type = "Visa";
+            long number = 2323321323212321;
+            short ccvCode = 080;
+            DateTime expDate = new DateTime(2021, 5, 1);
+            string note = "card for USA";
+
+            CreditCard card = new CreditCard(category,name,type,number,ccvCode,expDate,note);
             Assert.IsNotNull(card);
         }
     }
