0ba679c2f0e57db5774743a6be553a618287563c
Renzo Matonti <matontirenzo@gmail.com>
2021-04-19 08:47:39 -0300

    code: Create UnitTest CreditCardTest RED TEST

---
+++ PasswordManager/PasswordManagerTest/creditCardTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class creditCardTest
+    {
+        [TestMethod]
+        public void CreateCreditCard()
+        {
+            creditCard card = new creditCard();
+            Assert.IsNotNull(card);
+        }
+    }
+}
