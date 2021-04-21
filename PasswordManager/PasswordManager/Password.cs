1e05bfdfdcd9deb9bd730e533aeb90e71a0cac71
Renzo Matonti <matontirenzo@gmail.com>
2021-04-21 18:09:28 -0300

    code: Added LastModificationDate Property to Password class

--- PasswordManager/PasswordManager/Password.cs
+++ PasswordManager/PasswordManager/Password.cs
@@ -9,17 +9,20 @@ namespace PasswordManager
     public class Password
     {
         public Category Category { get; set; }
-        public string Password { get; set; }
+        public string Pass { get; set; }
         public string Site { get; set; }
         public string User { get; set; }
         public string Note { get; set; }
+        public DateTime LastModificationDate { get; set; }
+
         public Password(Category category, string password, string site, string user, string note)
         {
             this.Category = category;
-            this.Password = password;
+            this.Pass = password;
             this.Site = site;
             this.User = user;
             this.Note = note;
+            this.LastModificationDate = DateTime.Today;
         }
     }
 }
