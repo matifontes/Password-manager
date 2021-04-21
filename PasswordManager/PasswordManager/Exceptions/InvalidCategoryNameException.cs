2fd62fc6415d9138091a1d10c39f1a72054ccf0f
Renzo Matonti <matontirenzo@gmail.com>
2021-04-21 16:34:45 -0300

    refactor: Create Exceptions folder

---
+++ PasswordManager/PasswordManager/Exceptions/InvalidCategoryNameException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PasswordManager
+{
+    public class InvalidCategoryNameException : Exception
+    {
+        public InvalidCategoryNameException(string message)
+            : base(message)
+        { 
+        }
+    }
+}
