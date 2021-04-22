a07d65ee925ac43154d8e856e88f1dc71f26f866
Matias Fontes <matifontes13@gmail.com>
2021-04-22 13:48:56 -0300

    code: Added validation for Credit Card Number property and IsValidNumber

--- PasswordManager/PasswordManager/creditCard.cs
+++ PasswordManager/PasswordManager/creditCard.cs
@@ -4,11 +4,25 @@ namespace PasswordManager
 {
     public class CreditCard
     {
-
+        const int LENGTH_FOR_VALID_NAME = 16;
         public Category CreditCardCategory { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-        public long Number { get; set; }
+        public long Number
+        {
+            get { return Number; }
+            private set
+            {
+                if (!IsValidNumber(value))
+                {
+                    throw new InvalidCreditCardNumberException();
+                }
+                else
+                {
+                    this.Number = value;
+                }
+            }
+        }
         public short CCVCode { get; set; }
         public DateTime ExpiryDate { get; set; }
         public string Note { get; set; }
@@ -22,6 +36,11 @@ namespace PasswordManager
             this.CCVCode = ccvCode;
             this.ExpiryDate = expDate;
             this.Note = note;
+        }
+
+        public bool IsValidNumber(long value)
+        {
+            return false;
         }
 
     }
