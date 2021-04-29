520f08f18d160fba4f7b17efc4043fb5a6518157
Matias Fontes <matifontes13@gmail.com>
2021-04-29 15:34:22 -0300

    code: Create GeneratePasswordSettings RED TEST

---
+++ PasswordManager/PasswordManagerTest/PasswordGenerationSettingsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PasswordManager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class PasswordGenerationSettingsTest
+    {
+        [TestMethod]
+        public void GeneratePasswordSettings()
+        {
+            GeneratePasswordSettings settings = new GeneratePasswordSettings();
+            Assert.IsNotNull(settings);
+        }
+    }
+}
