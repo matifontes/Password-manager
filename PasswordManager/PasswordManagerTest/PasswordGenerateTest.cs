b947d810be190620db0d115942e83494ee311824
Matias Fontes <matifontes13@gmail.com>
2021-04-30 20:15:35 -0300

    code: Test initialize implementation on PasswordGenerateTest

--- PasswordManager/PasswordManagerTest/PasswordGenerateTest.cs
+++ PasswordManager/PasswordManagerTest/PasswordGenerateTest.cs
@@ -7,6 +7,19 @@ namespace PasswordManagerTest
     [TestClass]
     public class PasswordGenerateTest
     {
+        private GeneratePasswordSettings settingsAllTrue;
+        int passwordLength = 10;
+        bool includeLowerCase = true;
+        bool includeUpperCase = true;
+        bool includeNumbers = true;
+        bool includeSpecialCharacters = true;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            settingsAllTrue = new GeneratePasswordSettings(passwordLength, includeLowerCase, includeUpperCase, includeNumbers, includeSpecialCharacters);
+        }
+
         [TestMethod]
         public void GeneratePassword()
         {
