ad1cc80bd4c9f7bd341430134c307e6b7f8192ca
Matias Fontes <matifontes13@gmail.com>
2021-04-30 20:13:26 -0300

    code: Basic implementation of GeneratePassword GREEN TEST

---
+++ PasswordManager/PasswordManager/PasswordGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager
+{
+    public static class PasswordGenerator
+    {
+
+        public static string GeneratePassword()
+        {
+            return "";
+        }
+    }
+}
