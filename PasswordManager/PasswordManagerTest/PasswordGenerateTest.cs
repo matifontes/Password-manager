63a875a70934c2fca0d97dd01f6aa2ba27aa5800
Matias Fontes <matifontes13@gmail.com>
2021-04-30 20:12:57 -0300

    code: Create GeneratePassword Test Method on PasswordGenerateTest

---
+++ PasswordManager/PasswordManagerTest/PasswordGenerateTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PasswordManager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class PasswordGenerateTest
+    {
+        [TestMethod]
+        public void GeneratePassword()
+        {
+            string password = PasswordGenerator.GeneratePassword();
+            Assert.IsNotNull(password);
+        }
+    }
+}
