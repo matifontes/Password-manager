229209e28ce0e1c8a800539eb77892a67804907a
Matias Fontes <matifontes13@gmail.com>
2021-05-02 13:19:41 -0300

    code: Created PasswordIsValid() Method [GREEN TEST]

--- PasswordManager/PasswordManager/PasswordGenerator.cs
+++ PasswordManager/PasswordManager/PasswordGenerator.cs
@@ -20,5 +20,20 @@ namespace PasswordManager
             }
             return string.Join(null, password);
         }
+
+        public static bool PasswordIsValid(GeneratePasswordSettings settings, string password)
+        {
+            const string LOWERCASE = @"[a-z]";
+            const string UPPERCASE = @"[A-Z]";
+            const string NUMERIC = @"[\d]";
+            const string SPECIAL = @"([!#$%&.*@\\])+";
+
+            bool lowerCaseIsValid = !settings.IncludeLowerCase || (settings.IncludeLowerCase && System.Text.RegularExpressions.Regex.IsMatch(password, LOWERCASE));
+            bool upperCaseIsValid = !settings.IncludeUpperCase|| (settings.IncludeUpperCase && System.Text.RegularExpressions.Regex.IsMatch(password, UPPERCASE));
+            bool numericIsValid = !settings.IncludeNumbers || (settings.IncludeNumbers && System.Text.RegularExpressions.Regex.IsMatch(password, NUMERIC));
+            bool specialIsValid = !settings.IncludeSpecialCharacters || (settings.IncludeSpecialCharacters && System.Text.RegularExpressions.Regex.IsMatch(password, SPECIAL));
+
+            return (lowerCaseIsValid && upperCaseIsValid && numericIsValid && specialIsValid);
+        }
     }
 }
