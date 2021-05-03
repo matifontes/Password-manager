5b51611c09d6deb328dcc74bb1ecbc8d52c16617
Renzo Matonti <matontirenzo@gmail.com>
2021-05-03 12:17:26 -0300

    code: Create Categories panel

---
+++ PasswordManager/UserInterface/Categories.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace UserInterface
+{
+    partial class Categories
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbxCategories = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(132, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Modificar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BtnModificar_Click);
+            // 
+            // lbxCategories
+            // 
+            this.lbxCategories.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxCategories.FormattingEnabled = true;
+            this.lbxCategories.ItemHeight = 32;
+            this.lbxCategories.Location = new System.Drawing.Point(17, 59);
+            this.lbxCategories.Name = "lbxCategories";
+            this.lbxCategories.Size = new System.Drawing.Size(333, 164);
+            this.lbxCategories.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Categorias";
+            // 
+            // Categories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxCategories);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Categories";
+            this.Size = new System.Drawing.Size(369, 299);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox lbxCategories;
+        private System.Windows.Forms.Label label1;
+    }
+}
