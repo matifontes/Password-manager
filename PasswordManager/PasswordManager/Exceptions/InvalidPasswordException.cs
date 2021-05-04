2e5fe05587d2ef314031de1616c151e6db1dd492
Matias Fontes <matifontes13@gmail.com>
2021-05-04 16:08:20 -0300

    code: Implementation of InvlaidPasswordException

---
+++ PasswordManager/PasswordManager/Exceptions/InvalidPasswordException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Exceptions
+{
+    public class InvalidPasswordException : Exception
+    {
+        public InvalidPasswordException()
+        {
+
+        }
+        public InvalidPasswordException(string message)
+            : base(message)
+        {
+        }
+    }
+}
