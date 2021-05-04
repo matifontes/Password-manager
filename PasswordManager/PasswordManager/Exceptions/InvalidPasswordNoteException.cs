ca35f5b6a2f063524a5399ec8e3ebfa125816012
Matias Fontes <matifontes13@gmail.com>
2021-05-04 16:23:55 -0300

    code: Created InvalidPasswordNoteException

---
+++ PasswordManager/PasswordManager/Exceptions/InvalidPasswordNoteException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Exceptions
+{
+    public class InvalidPasswordNoteException : Exception
+    {
+        public InvalidPasswordNoteException()
+        {
+
+        }
+        public InvalidPasswordNoteException(string message)
+            : base(message)
+        {
+        }
+    }
+}
