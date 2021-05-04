17073fda19b6b77ba5354462b1c0e7eee6c36b52
Renzo Matonti <matontirenzo@gmail.com>
2021-05-04 14:24:38 -0300

    code: Create PassworControllerTest RED TESTS

---
+++ PasswordManager/PasswordManagerTest/ProfileControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class ProfileControllerTest
+    {
+        [TestMethod]
+        public void CreateProfileWithPassword()
+        {
+            string password = "1234Hi@";
+            ProfileController profileController = new ProfileController(password);
+            Assert.IsTrue(profileController.ValidatePassword(password));
+        }
+
+        public void ValidateWithWrongPassword() 
+        {
+            string password = "1234Hi@";
+            string wrongPassword = "1234";
+            ProfileControllerTest profileController = new ProfileController(password);
+            Assert.IsFalse(profileController.ValidateWithWrongPassword(wrongPassword));
+        }
+    }
+}
