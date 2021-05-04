1cb10b766b0fe44379a7f52421be3a9669bb83d5
Renzo Matonti <matontirenzo@gmail.com>
2021-05-04 14:37:32 -0300

    code: Implementation of profile Controller GREEN TEST

---
+++ PasswordManager/PasswordManager/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿namespace PasswordManager
+{
+    public class ProfileController
+    {
+
+        private Profile profile;
+
+        public ProfileController(string password) {
+            this.profile = new Profile(password);
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            return this.profile.ValidateSystemPassword(password);
+        }
+    }
+}
