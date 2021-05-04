0783bcee5add578273b46af41057d0b792feff5a
Matias Fontes <matifontes13@gmail.com>
2021-05-04 15:29:19 -0300

    code: Created InvalidPasswordUserException

---
+++ PasswordManager/PasswordManager/Exceptions/InvalidPasswordUserException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Exceptions
+{
+    public class InvalidPasswordUserException : Exception
+    {
+        public InvalidPasswordUserException()
+        {
+
+        }
+        public InvalidPasswordUserException(string message)
+            : base(message)
+        {
+        }
+    }
+}
