3012f0817b36538d0b33c08b0419fc50e4624f39
Renzo Matonti <matontirenzo@gmail.com>
2021-05-04 15:17:04 -0300

    code: Implemented ChangePassword on profileController GREEN TEST

--- PasswordManager/PasswordManager/ProfileController.cs
+++ PasswordManager/PasswordManager/ProfileController.cs
@@ -1,4 +1,6 @@
-﻿namespace PasswordManager
+﻿using System;
+
+namespace PasswordManager
 {
     public class ProfileController
     {
@@ -12,6 +14,11 @@
         public bool ValidatePassword(string password)
         {
             return this.profile.ValidateSystemPassword(password);
+        }
+
+        public void ChangePassword(string actualpassword, string newpassword)
+        {
+            this.profile.ChangePassword(actualpassword, newpassword);
         }
     }
 }
