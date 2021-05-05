4057d57b619df7bb885cbd9296fc19ccdd111dee
Renzo Matonti <matontirenzo@gmail.com>
2021-05-05 15:31:34 -0300

    code: Add Test Initialize and cleanup to PasswordRepositoryTest

--- PasswordManager/PasswordManagerTest/PasswordRepositoryTest.cs
+++ PasswordManager/PasswordManagerTest/PasswordRepositoryTest.cs
@@ -7,44 +7,48 @@ namespace PasswordManagerTest
     [TestClass]
     public class PasswordRepositoryTest
     {
+        private PasswordRepository passwordRepository;
+        private Category category;
+        private Password password;
+        private string categoryName = "Personal";
+        private string pass = "Admin";
+        string site = "site";
+        string user = "root";
+        string note = "note";
+
+        [TestInitialize]
+        public void Setup() 
+        {
+            category = new Category(categoryName);
+            password = new Password(category,pass,site,user,note);
+            passwordRepository = new PasswordRepository();
+        }
+
+        [TestCleanup]
+        public void Cleanup() 
+        {
+            category = null;
+            password = null;
+            passwordRepository = null;
+        }
+
         [TestMethod]
         public void CreatePasswordsRepository()
         {
-            PasswordRepository passwordRepository = new PasswordRepository();
             Assert.IsNotNull(passwordRepository);
         }
 
         [TestMethod]
         public void AddPasswordToRepository() 
         {
-            PasswordRepository passwordRepository = new PasswordRepository();
-            string categorie = "Trabajo";
-            string pass = "Admin";
-            string site = "site";
-            string user = "root";
-            string note = "Note";
-            Category category = new Category(categorie);
-            Password password = new Password(category,pass,site,user,note);
-
             passwordRepository.AddPassword(password);
-
             Assert.AreEqual(passwordRepository.Count(), 1);
         }
 
         [TestMethod]
         public void RemovePasswordFromRepository() 
         {
-            PasswordRepository passwordRepository = new PasswordRepository();
-            string categorie = "Trabajo";
-            string pass = "Admin";
-            string site = "site";
-            string user = "root";
-            string note = "Note";
-            Category category = new Category(categorie);
-            Password password = new Password(category, pass, site, user, note);
-
             passwordRepository.AddPassword(password);
-
             passwordRepository.RemovePassword(password);
 
             Assert.AreEqual(passwordRepository.Count(), 0);
