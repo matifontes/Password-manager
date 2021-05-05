f77e1e92497b24a41e008c0e7a1d0d05839bbfcb
Renzo Matonti <matontirenzo@gmail.com>
2021-05-05 15:22:22 -0300

    code: PasswordRepository implementation

---
+++ PasswordManager/PasswordManager/PasswordRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace PasswordManager
+{
+    public class PasswordRepository
+    {
+
+        private List<Password> passwords;
+        public PasswordRepository() 
+        {
+            passwords = new List<Password>();        
+        }
+
+        public void AddPassword(Password password) 
+        {
+            this.passwords.Add(password);
+        }
+
+        public void RemovePassword(Password password) 
+        {
+            this.passwords.Remove(password);
+        }
+
+        public int Count() {
+            return this.passwords.Count;
+        }
+    }
+}
