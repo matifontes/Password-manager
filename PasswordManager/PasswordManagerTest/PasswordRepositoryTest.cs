5bc4d2792d071904cebffd8dfaf65ac594387593
Renzo Matonti <matontirenzo@gmail.com>
2021-05-05 10:12:28 -0300

    code: Create UnitTest PasswordRepositoryTest

---
+++ PasswordManager/PasswordManagerTest/PasswordRepositoryTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PasswordManager;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class PasswordRepositoryTest
+    {
+        [TestMethod]
+        public void CreatePasswordsRepository()
+        {
+            PasswordRepository passwordRepository = new PasswordRepository();
+            Assert.IsNotNull(passwordRepository);
+        }
+
+        [TestMethod]
+        public void AddPasswordToRepository() 
+        {
+            PasswordRepository passwordRepository = new PasswordRepository();
+            string categorie = "Trabajo";
+            string pass = "Admin";
+            string site = "site";
+            string user = "root";
+            string note = "Note";
+            Category category = new Category(categorie);
+            Password password = new Password(category,pass,site,user,note);
+
+            passwordRepository.AddPassword(password);
+
+            Assert.AreEqual(passwordRepository.passwords.count(), 1);
+        }
+
+        [TestMethod]
+        public void RemovePasswordFromRepository() 
+        {
+            PasswordRepository passwordRepository = new PasswordRepository();
+            string categorie = "Trabajo";
+            string pass = "Admin";
+            string site = "site";
+            string user = "root";
+            string note = "Note";
+            Category category = new Category(categorie);
+            Password password = new Password(category, pass, site, user, note);
+
+            passwordRepository.AddPassword(password);
+
+            passwordRepository.Remove(password);
+
+            Assert.AreEqual(passwordRepository.passwords.count(), 0);
+        }
+    }
+}
