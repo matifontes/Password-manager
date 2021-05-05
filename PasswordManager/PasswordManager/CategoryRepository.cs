2faad39202a97f1f9c32db924bd81589a7c148cb
Renzo Matonti <matontirenzo@gmail.com>
2021-05-05 16:46:48 -0300

    code: Implemented CategoryRepository

---
+++ PasswordManager/PasswordManager/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace PasswordManager
+{
+    public class CategoryRepository
+    {
+        private List<Category> categories;
+        public CategoryRepository() 
+        {
+            categories = new List<Category>();
+        }
+
+        public void AddCategory(Category category) 
+        {
+            categories.Add(category);
+        }
+
+        public void RemoveCategory(Category category) 
+        {
+            categories.Remove(category);
+        }
+
+        public int Count() 
+        {
+            return categories.Count;
+        }
+    }
+}
