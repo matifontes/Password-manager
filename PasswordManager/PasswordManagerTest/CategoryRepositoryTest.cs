15840110496b2713cec717f93b1404142f12165b
Renzo Matonti <matontirenzo@gmail.com>
2021-05-05 16:37:23 -0300

    code: Create CategoryRepositotyTest RED TESTS

---
+++ PasswordManager/PasswordManagerTest/CategoryRepositoryTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PasswordManager;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class CategoryRepositoryTest
+    {
+        private Category category;
+        private CategoryRepository categoryRepository;
+
+        [TestInitialize]
+        public void Setup() 
+        {
+            category = new Category("Personal");
+            categoryRepository = new CategoryRepository();
+        }
+
+        [TestCleanup]
+        public void Cleanup() 
+        {
+            category = null;
+            categoryRepository = null;
+        }
+
+        [TestMethod]
+        public void CreateCategoryRepository()
+        {
+            Assert.IsNotNull(categoryRepository);
+        }
+
+        [TestMethod]
+        public void AddCategory() 
+        {
+            categoryRepository.AddCategory(category);
+
+            Assert.AreEqual(categoryRepository.Count(), 1);
+        }
+
+        public void RemoveCategory() 
+        {
+            categoryRepository.AddCategory(category);
+
+            categoryRepository.RemoveCategory(category);
+
+            Assert.AreEqual(categoryRepository.Count(), 0);
+        }
+    }
+}
