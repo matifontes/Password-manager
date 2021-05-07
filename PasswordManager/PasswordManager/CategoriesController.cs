19164244314dcfb2a38c2a6880238935d20b232d
Renzo Matonti <matontirenzo@gmail.com>
2021-05-07 18:15:08 -0300

    code: Implemented CategoriesController Class GREEN TEST

---
+++ PasswordManager/PasswordManager/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace PasswordManager
+{
+    public class CategoriesController
+    {
+        private CategoryRepository categories;
+
+        public CategoriesController() 
+        {
+            categories = new CategoryRepository();
+        }
+
+        public void AddCategory(Category category) 
+        {
+            this.categories.AddCategory(category);  
+        }
+
+        public void RemoveCategory(Category category) 
+        {
+            this.categories.RemoveCategory(category);
+        }
+
+        public int Count() 
+        {
+           return this.categories.Count();
+        }
+
+        public List<Category> ListCategories() 
+        {
+            return this.categories.ListCategories();
+        }
+    }
+}
