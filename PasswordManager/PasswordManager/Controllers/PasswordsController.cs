b1e8c143666fb4d0d02143f589baeddd22df75cc
Renzo Matonti <matontirenzo@gmail.com>
2021-05-09 03:59:24 -0300

    code: Implemented PasswordsController GREEN TESTS

---
+++ PasswordManager/PasswordManager/Controllers/PasswordsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace PasswordManager
+{
+    public class PasswordsController
+    {
+        private PasswordRepository passwords;
+
+        public PasswordsController(PasswordRepository passwords)
+        {
+            this.passwords = passwords;
+        }
+
+        public void AddPassword(Password password) 
+        {
+            this.passwords.AddPassword(password);
+        }
+
+        public void RemovePassword(Password password) 
+        {
+            this.passwords.RemovePassword(password);
+        }
+
+        public int Count() 
+        {
+            return this.passwords.Count();
+        }
+
+        public List<Password> ListPasswords() 
+        {
+            return this.passwords.ListPasswords();
+        }
+    }
+}
