c5071b66c713a340f80819ddfb297b9c61b92330
Matias Fontes <matifontes13@gmail.com>
2021-05-10 11:34:26 -0300

    code: Created CreditCardControllerTest.cs and implementation of CreateCreditCardController() Test Method RED TEST

---
+++ PasswordManager/PasswordManagerTest/CreditCardControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordManager;
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class CreditCardControllerTest
+    {
+        [TestMethod]
+        public void CreateCreditCardController()
+        {
+            CreditCardRepository creditCards = new CreditCardRepository();
+            CreditCardController creditCardsController = new CreditCardsController(creditCards);
+            Assert.IsNotNull(creditCardsController);
+        }
+    }
+}
