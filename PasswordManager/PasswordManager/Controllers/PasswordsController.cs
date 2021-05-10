c257ed723f4c0ee5b7252e99d9ab972587892247
Matias Fontes <matifontes13@gmail.com>
2021-05-10 16:12:31 -0300

    code: Implemented methods to list passwords by strength on PasswordController GREEN TEST

--- PasswordManager/PasswordManager/Controllers/PasswordsController.cs
+++ PasswordManager/PasswordManager/Controllers/PasswordsController.cs
@@ -35,5 +35,39 @@ namespace PasswordManager.Controllers
         {
             return this.passwords.ListPasswords();
         }
+
+        public List<Password> ListRedPasswords()
+        {
+            List<Password> listRedPasswords = this.passwords.ListRedPasswords();
+            this.passwords.SortListByCategoryName(listRedPasswords);
+            return listRedPasswords;
+        }
+
+        public List<Password> ListOrangePasswords()
+        {
+            List<Password> listOrangePasswords = this.passwords.ListOrangePasswords();
+            this.passwords.SortListByCategoryName(listOrangePasswords);
+            return listOrangePasswords;
+        }
+        public List<Password> ListYellowPasswords()
+        {
+            List<Password> listYellowPasswords = this.passwords.ListYellowPasswords();
+            this.passwords.SortListByCategoryName(listYellowPasswords);
+            return listYellowPasswords;
+        }
+        public List<Password> ListLightGreenPasswords()
+        {
+            List<Password> listLightGreenPasswords = this.passwords.ListLGreenPasswords();
+            this.passwords.SortListByCategoryName(listLightGreenPasswords);
+            return listLightGreenPasswords;
+        }
+
+        public List<Password> ListDarkGreenPasswords()
+        {
+            List<Password> listDarkGreenPasswords = this.passwords.ListDGreenPasswords();
+            this.passwords.SortListByCategoryName(listDarkGreenPasswords);
+            return listDarkGreenPasswords;
+        }
+
     }
 }
