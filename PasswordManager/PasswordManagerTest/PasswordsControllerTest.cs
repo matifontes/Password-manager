0f837c0b317481e817ccc0b23ae5f3616c44fa0b
Matias Fontes <matifontes13@gmail.com>
2021-05-12 09:45:07 -0300

    code: Created test methods to PasswordExist on Controller

--- PasswordManager/PasswordManagerTest/PasswordsControllerTest.cs
+++ PasswordManager/PasswordManagerTest/PasswordsControllerTest.cs
@@ -174,5 +174,24 @@ namespace PasswordManagerTest
 
             Assert.AreEqual(passwordsResult[0], password);
         }
+
+        [TestMethod]
+        public void PasswordExist()
+        {
+            List<Password> passList = new List<Password>();
+            passList.Add(password);
+            string pass = password.Pass;
+            Assert.IsTrue(passwordsController.PasswordExist(passList, pass));
+        }
+
+
+        [TestMethod]
+        public void PasswordNotExist()
+        {
+            List<Password> passList = new List<Password>();
+            passList.Add(password);
+            string pass = "dfault";
+            Assert.IsTrue(passwordsController.PasswordExist(passList, pass));
+        }
     }
 }
