e3dc00b457fc60dedbd40a3f7d54b920da9be891
Renzo Matonti <matontirenzo@gmail.com>
2021-05-13 09:35:40 -0300

    code: Added More special chars to password Generate

--- PasswordManager/PasswordManager/Domain/PasswordGenerator.cs
+++ PasswordManager/PasswordManager/Domain/PasswordGenerator.cs
@@ -84,7 +84,7 @@ namespace PasswordManager
 
         private static bool ValidateSpecialCharacters(GeneratePasswordSettings settings, string password)
         {
-            const string SPECIAL = @"([!#$%&.*@\\])+";
+            const string SPECIAL = @"([!#$%&.*@\\,:;\[\])(}{^\|¿\?=~< >])+";
             bool isValid = !settings.IncludeSpecialCharacters || (settings.IncludeSpecialCharacters && Regex.IsMatch(password, SPECIAL));
             return isValid;
         }
