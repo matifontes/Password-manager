f64910f1ff5c1fc65f51fe7c2e4a7628c0d1b828
Renzo Matonti <matontirenzo@gmail.com>
2021-06-06 17:13:44 -0300

    code: PasswordManagerDB Context added

---
+++ PasswordManager/PasswordManagerDataLeyer/PasswordManagerContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using PasswordManager;
+
+namespace PasswordManagerDataLeyer
+{
+    class PasswordManagerContext : DbContext
+    {
+        public PasswordManagerContext() : base("name=PasswordManagerDB") 
+        {
+            
+        }
+
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Password> Passwords { get; set; }
+        public DbSet<CreditCard> CreditCards { get; set; }
+
+    }
+}
