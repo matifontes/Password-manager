edca2dfb32d3320789c7181db34ecee2f37b9138
Matias Fontes <matifontes13@gmail.com>
2021-06-07 10:59:42 -0300

    code: Implementated DataBreachesRepository [GREEN TEST]

---
+++ PasswordManager/PasswordManager/Repositories/DataBreachesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Repositories
+{
+    public class DataBreachesRepository
+    {
+        private List<DataBreach> dataBreaches;
+
+        public DataBreachesRepository()
+        {
+            this.dataBreaches = new List<DataBreach>();
+        }
+    }
+}
