228051d9f3968edb93b483173bce90005e355840
Matias Fontes <matifontes13@gmail.com>
2021-06-07 11:28:39 -0300

    code: Implementated CreateNewDataBreachesRepositoryShouldBeEmpty TestMethod

---
+++ PasswordManager/PasswordManagerTest/DataBreachesRepositoryTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using PasswordManager;
+using PasswordManager.Repositories;
+using PasswordManager.Exceptions;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class DataBreachesRepositoryTest
+    {
+        [TestMethod]
+        public void CreateDataBreachesRepository()
+        {
+            DataBreachesRepositoryTest dataBreaches = new DataBreachesRepositoryTest();
+            Assert.IsNotNull(dataBreaches);
+        }
+
+
+        [TestMethod]
+        public void CreateNewDataBreachesRepositoryShouldBeEmpty()
+        {
+            List<CreditCard> cards = new List<CreditCard>();
+            List<Password> passwords = new List<Password>();
+            DataBreach dBreach = new DataBreach(cards, passwords);
+
+            DataBreachesRepository dataBreachesRepository = new DataBreachesRepository();
+            Assert.IsTrue(dataBreachesRepository.IsEmpty());
+        }
+
+    }
+}
