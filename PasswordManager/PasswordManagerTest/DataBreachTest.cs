c320846fd435f235101dd993829f69cde8e2084c
Matias Fontes <matifontes13@gmail.com>
2021-06-07 10:35:54 -0300

    code: Created a DataBreachTest and create data breach test method

---
+++ PasswordManager/PasswordManagerTest/DataBreachTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordManager;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class DataBreachTest
+    {
+        [TestMethod]
+        public void CreateDataBreach()
+        {
+            List<CreditCard> creditCards = new List<CreditCard>();
+            List<Password> passwords = new List<Password>();
+
+            DataBreach dBreachTest = new DataBreach(creditCards, passwords);
+        }
+    }
+}
