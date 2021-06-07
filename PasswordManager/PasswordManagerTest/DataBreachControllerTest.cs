df8071d0d44caef3c4a6bd045f0af3911031237f
Matias Fontes <matifontes13@gmail.com>
2021-06-07 17:49:45 -0300

    code: Create DataBreachController TestClass

---
+++ PasswordManager/PasswordManagerTest/DataBreachControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PasswordManager;
+using System.Collections.Generic;
+using PasswordManager.Controllers;
+using PasswordManager.Exceptions;
+using PasswordManager.Repositories;
+
+namespace PasswordManagerTest
+{
+    [TestClass]
+    public class DataBreachControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
