f348e104c2cfef65c7553b330bd5f07a956e265d
Matias Fontes <matifontes13@gmail.com>
2021-06-07 11:32:42 -0300

    code: Implementated IsEmpty method on DataBreachRepository [GREEN TEST]

--- PasswordManager/PasswordManager/Repositories/DataBreachesRepository.cs
+++ PasswordManager/PasswordManager/Repositories/DataBreachesRepository.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PasswordManager.Exceptions;
 
 namespace PasswordManager.Repositories
 {
@@ -12,7 +10,12 @@ namespace PasswordManager.Repositories
 
         public DataBreachesRepository()
         {
-            this.dataBreaches = new List<DataBreach>();
+            dataBreaches = new List<DataBreach>();
+        }
+
+        public bool IsEmpty()
+        {
+           return this.dataBreaches.Count == 0;
         }
     }
 }
