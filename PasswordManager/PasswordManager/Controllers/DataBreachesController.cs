78e0b4e48918229478ecd70768129d689ceac181
Matias Fontes <matifontes13@gmail.com>
2021-06-07 17:55:49 -0300

    code: Implementated constructor of DataBreachesController [GREEN TEST]

---
+++ PasswordManager/PasswordManager/Controllers/DataBreachesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordManager.Repositories;
+
+namespace PasswordManager.Controllers
+{
+    public class DataBreachesController
+    {
+        private DataBreachesRepository dBreaches;
+
+        public DataBreachesController(DataBreachesRepository dBreaches)
+        {
+            this.dBreaches = dBreaches;
+        }
+    }
+}
