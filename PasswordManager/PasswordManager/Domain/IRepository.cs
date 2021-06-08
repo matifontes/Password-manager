7e3a8dc4c6c2b8654cafe614f185991d5c420c1a
Renzo Matonti <matontirenzo@gmail.com>
2021-06-08 16:40:06 -0300

    code: Create IRepository

---
+++ PasswordManager/PasswordManager/Domain/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        bool IsEmpty();
+        T Get(int Id);
+        IEnumerable<T> GetAll();
+        void Delete(int id);
+    }
+}
