fe9b1302166723f37df1087e4a46c804bf6b361f
Renzo Matonti <matontirenzo@gmail.com>
2021-06-13 16:36:09 -0300

    migration: Migrated Databreach to DB

--- PasswordManager/PasswordManagerDataLeyer/Migrations/202106062139543_PasswMngMigration.cs
+++ PasswordManager/PasswordManagerDataLeyer/Migrations/202106062139543_PasswMngMigration.cs
@@ -49,6 +49,18 @@
                 .Index(t => t.Profile_Id);
             
             CreateTable(
+                "dbo.DataBreachEntities",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                        Profile_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ProfileEntities", t => t.Profile_Id)
+                .Index(t => t.Profile_Id);
+            
+            CreateTable(
                 "dbo.PasswordEntities",
                 c => new
                     {
@@ -68,21 +80,60 @@
                 .Index(t => t.CategoryEntity_Id)
                 .Index(t => t.Profile_Id);
             
+            CreateTable(
+                "dbo.DataBreachEntityCreditCardEntities",
+                c => new
+                    {
+                        DataBreachEntity_Id = c.Int(nullable: false),
+                        CreditCardEntity_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.DataBreachEntity_Id, t.CreditCardEntity_Id })
+                .ForeignKey("dbo.DataBreachEntities", t => t.DataBreachEntity_Id, cascadeDelete: true)
+                .ForeignKey("dbo.CreditCardEntities", t => t.CreditCardEntity_Id, cascadeDelete: true)
+                .Index(t => t.DataBreachEntity_Id)
+                .Index(t => t.CreditCardEntity_Id);
+            
+            CreateTable(
+                "dbo.PasswordEntityDataBreachEntities",
+                c => new
+                    {
+                        PasswordEntity_Id = c.Int(nullable: false),
+                        DataBreachEntity_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.PasswordEntity_Id, t.DataBreachEntity_Id })
+                .ForeignKey("dbo.PasswordEntities", t => t.PasswordEntity_Id, cascadeDelete: true)
+                .ForeignKey("dbo.DataBreachEntities", t => t.DataBreachEntity_Id, cascadeDelete: true)
+                .Index(t => t.PasswordEntity_Id)
+                .Index(t => t.DataBreachEntity_Id);
+            
         }
         
         public override void Down()
         {
-            DropForeignKey("dbo.PasswordEntities", "Profile_Id", "dbo.ProfileEntities");
-            DropForeignKey("dbo.PasswordEntities", "CategoryEntity_Id", "dbo.CategoryEntities");
             DropForeignKey("dbo.CreditCardEntities", "Profile_Id", "dbo.ProfileEntities");
+            DropForeignKey("dbo.DataBreachEntities", "Profile_Id", "dbo.ProfileEntities");
+            DropForeignKey("dbo.PasswordEntities", "Profile_Id", "dbo.ProfileEntities");
+            DropForeignKey("dbo.PasswordEntityDataBreachEntities", "DataBreachEntity_Id", "dbo.DataBreachEntities");
+            DropForeignKey("dbo.PasswordEntityDataBreachEntities", "PasswordEntity_Id", "dbo.PasswordEntities");
+            DropForeignKey("dbo.PasswordEntities", "CategoryEntity_Id", "dbo.CategoryEntities");
+            DropForeignKey("dbo.DataBreachEntityCreditCardEntities", "CreditCardEntity_Id", "dbo.CreditCardEntities");
+            DropForeignKey("dbo.DataBreachEntityCreditCardEntities", "DataBreachEntity_Id", "dbo.DataBreachEntities");
             DropForeignKey("dbo.CreditCardEntities", "CategoryEntity_Id", "dbo.CategoryEntities");
             DropForeignKey("dbo.CategoryEntities", "ProfileEntity_Id", "dbo.ProfileEntities");
+            DropIndex("dbo.PasswordEntityDataBreachEntities", new[] { "DataBreachEntity_Id" });
+            DropIndex("dbo.PasswordEntityDataBreachEntities", new[] { "PasswordEntity_Id" });
+            DropIndex("dbo.DataBreachEntityCreditCardEntities", new[] { "CreditCardEntity_Id" });
+            DropIndex("dbo.DataBreachEntityCreditCardEntities", new[] { "DataBreachEntity_Id" });
             DropIndex("dbo.PasswordEntities", new[] { "Profile_Id" });
             DropIndex("dbo.PasswordEntities", new[] { "CategoryEntity_Id" });
+            DropIndex("dbo.DataBreachEntities", new[] { "Profile_Id" });
             DropIndex("dbo.CreditCardEntities", new[] { "Profile_Id" });
             DropIndex("dbo.CreditCardEntities", new[] { "CategoryEntity_Id" });
             DropIndex("dbo.CategoryEntities", new[] { "ProfileEntity_Id" });
+            DropTable("dbo.PasswordEntityDataBreachEntities");
+            DropTable("dbo.DataBreachEntityCreditCardEntities");
             DropTable("dbo.PasswordEntities");
+            DropTable("dbo.DataBreachEntities");
             DropTable("dbo.CreditCardEntities");
             DropTable("dbo.ProfileEntities");
             DropTable("dbo.CategoryEntities");
